06110a7ab4a6898a3077b5cc3f60a2a0b33b0fde
Diyan Apostolov <diyan.apostolov@yahoo.com>
2022-07-11 09:52:10 +0300

    Add homework for Conditional Statements

---
+++ 02.ConditionalStatements-Exercise/07.Shopping/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _07.Shopping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int countOfVideoCards = int.Parse(Console.ReadLine());
+            int CountOfProcessor = int.Parse(Console.ReadLine());
+            int countOfRamMemory = int.Parse(Console.ReadLine());
+
+            double priceForVideoCard = 250;
+            double sumVideoCards = countOfVideoCards * priceForVideoCard;
+
+            double pricePerProcessor = sumVideoCards * 0.35;
+            double sumProcessor = CountOfProcessor * pricePerProcessor;
+
+            double pricePerRam = sumVideoCards * 0.10;
+            double sumRam = pricePerRam * countOfRamMemory;
+
+            double fullPrice = sumProcessor + sumRam + sumVideoCards;
+
+            if (countOfVideoCards > CountOfProcessor)
+            {
+                fullPrice = fullPrice - (fullPrice * 0.15);
+            }
+            else
+            {
+                fullPrice = sumProcessor + sumRam + sumVideoCards;
+            }
+
+            if (budget >= fullPrice)
+            {
+                double moneyLeft = budget - fullPrice;
+                Console.WriteLine($"You have {moneyLeft:f2} leva left!");
+            }
+            else
+            {
+                double moneyNeeded = fullPrice - budget;
+                Console.WriteLine($"Not enough money! You need {moneyNeeded:f2} leva more!");
+            }
+        }
+    }
+}
